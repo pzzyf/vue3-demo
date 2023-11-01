8b5b9115ba89893dba0d234cf5a0a579a992eb84
pzzyf <2279948211@qq.com>
2023-11-01 10:18:48 +0800

    chore: use storeToRefs keep state active and use getter and action can use store

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,21 @@
 </template>
 
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
+import { storeToRefs } from 'pinia';
 import { useCounterStore } from './store/piniaTest.ts'
+import { watch } from 'vue';
 
 const store = useCounterStore()
 const { count } = storeToRefs(store)
 const { increment } = store
+
+watch(
+  count,
+  () => {
+    console.log(store.double);
+  },
+  {deep: true}
+)
 
 </script>
 
--- src/store/piniaTest.ts
+++ src/store/piniaTest.ts
@@ -3,10 +3,13 @@ import { defineStore } from "pinia";
 export const useCounterStore = defineStore("counter", {
   state: () => ({ count: 1 }),
   getters: {
-    double: (state) => state.count * 2,
+    double: (state) => {
+      console.log('getters change');
+      return state.count * 2}
   },
   actions: {
     increment() {
+      console.log('actions change');
       this.count++;
     },
   },
