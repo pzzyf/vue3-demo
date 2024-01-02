07a323d0c0335a084648a266c1101417409a4893
pzzyf <2279948211@qq.com>
2024-01-02 09:57:59 +0800

    ！！

---
+++ src/components/jsts.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>1</div>
+</template>
+
+<script setup lang="ts">
+const a = true;
+console.log(!!a);
+</script>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
     path: "/",
-    component: () => import("../components/f-model.vue"),
+    component: () => import("../components/jsts.vue"),
   },
   // echarts
   {
